59c6c80cfa00292a874dce7149ea35a2c96214b1
Marcelino Matias de Souza <marcelinomatiasdesouza@gmail.com>
2019-04-05 17:25:55 -0300

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,11 @@
 const config = {
     headers: {
-        'access-token': 'VdD1IZTzTF9WwMUdnaSugg',
+        'access-token': 'abc',
         'Api-Version': 2,
-        'client': 'Lxef57L5fy2BbX1cCngrBA',
-        'token-type': 'Bearer',
-        'uid': 'mmatias@greenmile.com.br',
-        'uuid': '9dbdc432-115a-418c-8ba1-dbc220edb37a'
+        'client': 'abc',
+        'token-type': 'abc',
+        'uid': 'abc@abc.com.br',
+        'uuid': 'abc'
     }
 }
 
